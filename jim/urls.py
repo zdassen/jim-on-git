1d86e1080a71e2097566390e2cda9985faf2ace3
zdassen <e3eas9lt1wuyifw@yahoo.co.jp>
2018-06-06 23:00:48 +0900

    write CRUD for Article

--- jim/urls.py
+++ jim/urls.py
@@ -1,8 +1,41 @@
 # jim/urls.py
 from django.urls import path
 
+from . import views
+
 
 app_name = "jim"
 
 urlpatterns = [
+
+    # # # # # Article モデル # # # # #
+
+    # 記事(のネタ)の一覧
+    # ex: /jim/articles/
+    path("articles/",
+        views.ArticleIndexView.as_view(),
+        name="article-index"
+    ),
+
+    # 記事(のネタ)の詳細
+    # ex: /jim/articles/42/
+    path("articles/<int:pk>/",
+        views.ArticleDetailView.as_view(),
+        name="article-detail"
+    ),
+
+    # 記事(のネタ)の新規作成
+    # ex: /jim/articles/new/
+    path("articles/new/",
+        views.ArticleCreateView.as_view(),
+        name="article-new"
+    ),
+
+    # 記事(のネタ)の編集
+    # ex: /jim/articles/edit/42/
+    path("articles/edit/<int:pk>/",
+        views.ArticleEditView.as_view(),
+        name="article-edit"
+    ),
+
 ]
--- jim/views.py
+++ jim/views.py
@@ -1,4 +1,94 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Article, Point, Thought
 
 
 # Create your views here.
+class CreateView(generic.CreateView):
+    
+    def get_context_data(self, **kwargs):
+        """埋め込み変数をセットする"""
+        context = super().get_context_data(**kwargs)
+
+        # CRUD のタイプをセットする
+        context["crud_type"] = "new"
+
+        # ボタンの値をセットする
+        context["button_value"] = "新規作成"
+
+        return context
+
+
+class EditView(generic.UpdateView):
+    
+    def get_context_data(self, **kwargs):
+        """埋め込み変数をセットする"""
+        context = super().get_context_data(**kwargs)
+
+        # CRUD のタイプをセットする
+        context["crud_type"] = "edit"
+
+        # ボタンの値をセットする
+        context["button_value"] = "編集/更新"
+
+        return context
+
+
+class ArticleIndexView(generic.ListView):
+    """記事(ネタ)の一覧"""
+
+    # 対象のモデル
+    model = Article
+
+    # 使用するテンプレート
+    template_name = "jim/article/article_list.html"
+
+    def get_queryset(self):
+        """レコードを取得する"""
+        article_list = Article.objects.filter(
+            user=self.request.user
+        ).order_by("-id")
+
+        return article_list
+
+
+class ArticleDetailView(generic.DetailView):
+    """記事(ネタ)の詳細"""
+
+    # 対象のモデル
+    model = Article
+
+    # 使用するテンプレート
+    template_name = "jim/article/article_detail.html"
+
+
+class ArticleFormView(object):
+    """記事(ネタ)の新規作成/編集"""
+
+    # 対象のモデル
+    model = Article
+
+    # 編集対象のフィールド
+    fields = (
+        "title",
+    )
+
+    # 使用するテンプレート
+    template_name = "jim/article/article_form.html"
+
+    def form_valid(self, form):
+        """フォームの値が正常な場合の処理"""
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
+
+class ArticleCreateView(ArticleFormView, CreateView):
+    """記事(ネタ)の新規作成"""
+    pass
+
+
+class ArticleEditView(ArticleFormView, EditView):
+    """記事(ネタ)の編集"""
+    pass
